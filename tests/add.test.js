ab25a88e517bec8d044930d43563acdae1af5abd
Martti Grönholm <martti.gronholm@vincit.fi>
2022-12-15 10:41:23 +0200

    Add tests for add function

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,7 +1,19 @@
-const add = require('../src/add');
+import createMathOperation from '../src/add';
 
 describe('add.js', () => {
   test('add positive integers', () => {
-    expect(add(1,2)).toBe(3);
+    expect(createMathOperation(1,2)).toBe(3);
+  })
+
+  test('add negative integers', () => {
+    expect(createMathOperation(-1,-2)).toBe(-3);
+  })
+
+  test('add positive and negative integers', () => {
+    expect(createMathOperation(1,-2)).toBe(-1);
+  })
+
+  test('add floats', () => {
+    expect(createMathOperation(1.5,2.5)).toBe(4);
   })
 })
